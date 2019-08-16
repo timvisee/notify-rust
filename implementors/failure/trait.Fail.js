bb78f5a843fec5981760e7d857e18f252fb9543c
Hendrik Sollich <hendrik@hoodie.de>
2019-08-16 22:59:53 +0200

    doc for v3.6.2

---
+++ implementors/failure/trait.Fail.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors["failure"] = [];
+implementors["notify_rust"] = [{text:"impl <a class=\"trait\" href=\"failure/trait.Fail.html\" title=\"trait failure::Fail\">Fail</a> for <a class=\"enum\" href=\"notify_rust/error/enum.ErrorKind.html\" title=\"enum notify_rust::error::ErrorKind\">ErrorKind</a>",synthetic:false,types:["notify_rust::error::ErrorKind"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
